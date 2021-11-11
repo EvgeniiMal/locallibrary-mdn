62b82cd50c2b8c25a5fb5c19fb1fbb1f7cdd1c9b
Evgenii <eugenemal88@gmail.com>
2021-11-11 23:25:08 +0200

    feat: create admin classes for catalog models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+    def display_genre(self, obj):
+        return ', '.join([genre.name for genre in obj.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
