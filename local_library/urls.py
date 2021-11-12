d1883a1a2c2f2018afe888bd8a4c0d743d8214db
Evgenii <eugenemal88@gmail.com>
2021-11-12 17:10:39 +0200

    feat: added auth urls

--- local_library/urls.py
+++ local_library/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
 
