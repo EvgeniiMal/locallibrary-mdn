b99519aeb1fc44d77589f1a60d1f5c2b623f4846
Evgenii <eugenemal88@gmail.com>
2021-11-12 17:45:38 +0200

    feat: added debug-mode emain_backend console setting

--- local_library/settings.py
+++ local_library/settings.py
@@ -119,3 +119,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
