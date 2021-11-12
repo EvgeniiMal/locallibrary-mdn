c8f2855c4f75509a6d2af39dcf0b4f9015499911
Evgenii <eugenemal88@gmail.com>
2021-11-12 16:28:53 +0200

    feat: create session-based visit counter

--- catalog/views.py
+++ catalog/views.py
@@ -6,12 +6,13 @@ from .models import Book, Author, BookInstance, Genre
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_books_with_word_available = Book.objects.filter(title__contains='the').count()
     num_genres_with_word_available = Genre.objects.filter(name__contains='horror').count()
-
-    num_authors = Author.objects.count()
 
     context = {
         'num_books': num_books,
@@ -20,6 +21,7 @@ def index(request):
         'num_authors': num_authors,
         'num_books_with_word': num_books_with_word_available,
         'num_genres_with_word': num_genres_with_word_available,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
