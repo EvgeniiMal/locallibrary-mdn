613dd9f578892fb53a0cb925c2c341538b410b8d
Evgenii <eugenemal88@gmail.com>
2021-11-14 16:21:09 +0200

    fix: added permission policy to author and book manage views (based on standart django permissions)

--- catalog/views.py
+++ catalog/views.py
@@ -99,20 +99,23 @@ class AuthorDetailView(DetailView):
     model = Author
 
 
-class AuthorCreate(CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     initial = {'date_of_death': '11/06/2020'}
+    permission_required = 'catalog.create_author'
 
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = '__all__'
+    permission_required = 'catalog.update_author'
 
 
-class AuthorDelete(DeleteView):
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
 
 
 class BookListView(ListView):
@@ -124,16 +127,19 @@ class BookDetailView(DetailView):
     model = Book
 
 
-class BookCreate(CreateView):
+class BookCreate(PermissionRequiredMixin, CreateView):
     model = Book
     fields = ['title', 'author', 'summary', 'isbn', 'language', 'genre']
+    permission_required = 'catalog.create_book'
 
 
-class BookUpdate(UpdateView):
+class BookUpdate(PermissionRequiredMixin, UpdateView):
     model = Book
     fields = '__all__'
+    permission_required = 'catalog.change_book'
 
 
-class BookDelete(DeleteView):
+class BookDelete(PermissionRequiredMixin, DeleteView):
     model = Book
     success_url = reverse_lazy('books')
+    permission_required = 'catalog.delete_book'
